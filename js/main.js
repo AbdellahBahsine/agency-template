22fa5cdf7e6affedfdf6ed9f84a58a427865961b
Paylo01 <paylo_@outlook.com>
2021-04-06 19:17:03 +0100

    finished creating the layout and added some responsiveness

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const mobileMenu = document.getElementById("mobileMenu");
+const menuContent = document.getElementById("menuContent");
+
+mobileMenu.addEventListener("click", function() {
+    menuContent.classList.toggle("open")
+}) 
+
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         items: 1,
