79de48b1eec84b535d3c71bf048b07fccfd5c58f
Paylo01 <paylo_@outlook.com>
2021-04-07 16:07:30 +0100

    created search form

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 const mobileMenu = document.getElementById("mobileMenu");
 const menuContent = document.getElementById("menuContent");
 
+const searchBtn = document.getElementById("search-icon");
+const searchForm = document.getElementById("search-form")
+
 mobileMenu.addEventListener("click", function() {
     menuContent.classList.toggle("open")
+}) 
+
+searchBtn.addEventListener("click", function() {
+    searchForm.classList.toggle("active")
 }) 
 
 $(document).ready(function(){
@@ -12,3 +19,32 @@ $(document).ready(function(){
         mouseDrag: false
     });
   });
+
+  const categoryBtns = document.getElementsByClassName("category");
+
+for(var i = 0; i < categoryBtns.length; i++){
+
+    const categoryBtns = document.getElementsByClassName("category");
+
+    categoryBtns[i].addEventListener("click", function() {
+
+        const categoryBtns = document.getElementsByClassName("category");
+    
+        const attr = categoryBtns[i].getAttribute('data-category');
+    
+        let items = document.getElementsByClassName("item");
+        items[i].style.display = "none";
+    
+        if(attr == "branding"){
+            attr.style.display = "flex";
+        } else if(attr == "mockup") {
+            attr.style.display = "flex";
+        } else if(attr == "website") {
+            attr.style.display = "flex";
+        } else if(attr == "ui/ux") {
+            attr.style.display = "flex";
+        } else{
+            items.style.display = "flex";
+        }
+    })
+}
